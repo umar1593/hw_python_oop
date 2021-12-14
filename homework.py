2527b4514412f9a2a4cf424819f47bff864c4e09
Umar Shirvaniev <umar1593@mail.ru>
2021-12-14 20:33:23 +0300

    the end... maybe

--- homework.py
+++ homework.py
@@ -15,10 +15,10 @@ class InfoMessage:
 
     def get_message(self) -> str:
         return(f'Тип тренировки: {self.training_type};'
-               f'Длительность: {self.duration:.3f} ч.;'
-               f'Дистанция: {self.distance:.3f} км;'
-               f'Ср. скорость: {self.speed:.3f} км/ч;'
-               f'Потрачено ккал: {self.calories:.3f}.')
+               f' Длительность: {self.duration:.3f} ч.;'
+               f' Дистанция: {self.distance:.3f} км;'
+               f' Ср. скорость: {self.speed:.3f} км/ч;'
+               f' Потрачено ккал: {self.calories:.3f}.')
 
 
 class Training:
@@ -60,31 +60,33 @@ class Training:
 
 class Running(Training):
     """Тренировка: бег."""
+    COEFF_CALORIE_1: int = 18
+    COEFF_CALORIE_2: int = 20
+    
     def __init__(self, action: int, duration: float, weight: float) -> None:
         super().__init__(action, duration, weight)
 
     def get_spent_calories(self) -> float:
-        COEFF_CALORIE_1 = 18
-        COEFF_CALORIE_2 = 20
-        return(((COEFF_CALORIE_1 * self.get_mean_speed()
-                 - COEFF_CALORIE_2) * self.weight)
+        return(((self.COEFF_CALORIE_1 * self.get_mean_speed()
+                 - self.COEFF_CALORIE_2) * self.weight)
                / self.M_IN_KM * self.duration * 60)
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
+    COEFF_1: float = 0.035
+    COEFF_2: float = 0.029
+    SQUARE: int = 2
+
     def __init__(self, action: int, duration: float,
                  weight: float, height: float) -> None:
         super().__init__(action, duration, weight)
         self.height = height
 
     def get_spent_calories(self) -> float:
-        COEFF_1 = 0.035
-        COEFF_2 = 0.029
-        SQUARE = 2
-        return((COEFF_1 * self.weight + (self.get_mean_speed()**SQUARE
+        return((self.COEFF_1 * self.weight + (self.get_mean_speed()**self.SQUARE
                 // self.height)
-                * COEFF_2 * self.weight) * self.duration * 60)
+                * self.COEFF_2 * self.weight) * self.duration * 60)
 
 
 class Swimming(Training):
@@ -132,4 +134,3 @@ if __name__ == '__main__':
     for workout_type, data in packages:
         training = read_package(workout_type, data)
         main(training)
-vjhvkjhvkhjv,v,m,v
